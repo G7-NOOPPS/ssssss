1623f7bc40ad78fb61114e7094ae79d3c0ea6ca4
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-03-16 10:52:11 -0300

    Add files via upload

---
+++ gt/horas.js
@@ -0,0 +1,15 @@
+let valorPorHora = 50;
+let horas Trabalhadas 8;
+
+console.log("--- Resumo do Expediente ---");
+console.log(Horas iniciais: ${horas Trabalhadas}h);
+
+horas Trabalhadas++;
+console.log("Após hora extra Moras Trabalhadas++): $(horas Trabalhadash");
+
+horas Trabalhadas--
+console.log('Após saída antecipada (horas Trabalhadas--): ${horas Trabalhadas}h);
+
+
+let taxaSistema = 15;
+let pagamentoTotal = (horas Trabalhadas valor PorHora) taxaSistema; 36
