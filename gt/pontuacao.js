592a69a3d0789164c50ba846ea50a27664c07029
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-04-07 11:51:50 +0000

    versao 1

---
+++ gt/pontuacao.js
@@ -0,0 +1,11 @@
+const nomeJogador = "gt";
+let faseAtual = 3;
+let hp ="2";
+
+let pontosPorMissao = [80, 85, 90, 100];
+
+//pontosPorMissao.length
+let pontuacaoTotal = 0;
+
+console.log(`seja bem vindo: ${nomeJogador}!` );
+console.log(pontosPorMissao);
