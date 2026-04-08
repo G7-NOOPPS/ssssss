bdcd5c0385bc5f815655529fa3d2dbb80966e5ce
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-04-08 12:32:55 +0000

    versao 2

--- gt/pontuacao.js
+++ gt/pontuacao.js
@@ -7,5 +7,12 @@ let pontosPorMissao = [80, 85, 90, 100];
 //pontosPorMissao.length
 let pontuacaoTotal = 0;
 
+for (let i = 0; i < pontuacoes.length; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+let media = pontuacaoTotal / pontuacoes.length;
+
+let passouDeFase = (media >= 75 && vidas > 0) || ((fase === 1))
+
 console.log(`seja bem vindo: ${nomeJogador}!` );
 console.log(pontosPorMissao);
