74056c9bbc8f0be9ce2d2d3b383cc0d48e6172e5
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-04-14 11:41:51 +0000

    v3

---
+++ gt/array1.js
@@ -0,0 +1,12 @@
+const frutas = ["Maçã", "Banana", "uva", "manga"];
+console.log(frutas[1]);
+console.log(frutas[3]);
+console.log(frutas.length);
+frutas[1] = "morango";
+frutas.push("pessego");
+console.log(frutas);
+console.log(frutas.length);
+
+frutas[3] = "abacate"
+frutas.push("melao")
+console.log(frutas)
