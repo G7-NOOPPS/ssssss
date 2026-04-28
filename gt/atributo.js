fe4502ff1023c4c8d7cc15f37cafb3293b604a11
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-04-28 11:19:34 +0000

    v1

---
+++ gt/atributo.js
@@ -0,0 +1,18 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(numero);
+
+let numeroN = 2;
+numeroN *= 2;
+console.log(numeroN);
+
+const idadeDandara = 26;
+const Minhaidade = 17;
+
+
+const Saoiguais = idadeDandara == Minhaidade;
+console.log('As idades são iguais? ' + Saoiguais )
+
+const comparacao = idadeDandara > Minhaidade;
+console.log('A dandara é mais velha ?' + comparacao);
