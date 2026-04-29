047202ba6baed28fff41c76e02ef41661338b505
G7-NOOPPS <luis.oliveira.silva07@escola.pr.gov.br>
2026-04-29 12:19:09 +0000

    v1

---
+++ gt/objeto.js
@@ -0,0 +1,18 @@
+const bancoAutomotivo = {
+    material: "Couro Natural",
+    cor: "Caramelo",
+    tamanho: "Padrão",
+    concluido: false
+}
+
+
+   
+const papelaria = {
+    caneta: "vermelha",
+    papel: "colorido",
+    lapis: "12 cores",
+    caderno: "48 paginas"
+    mochila: "preto"
+}
+
+console.log("o material escolhido daa papelaria, e uma caixa de lapis de", papelaria.lapis);
